88b2117030ea136268a131cab31e1576bf7cbe90
Risshub <rishavsubedi5@gmail.com>
2024-12-31 16:38:17 +0545

    Added PrivateRoute component for authentication protection.

--- task-manager/src/routes/Router.js
+++ task-manager/src/routes/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from '../pages/Login';
 import Signup from '../pages/Signup';
 import Profile from '../pages/Profile';
+import PrivateRoute from '../components/utils/PrivateRoute';
 
 const AppRouter = () => {
   return (
@@ -10,7 +11,7 @@ const AppRouter = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
       </Routes>
     </Router>
   );
