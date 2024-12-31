4c059a7b4a2b01edc81838b50d60259062305789
Risshub <rishavsubedi5@gmail.com>
2024-12-31 16:35:44 +0545

    Added Login page

---
+++ task-manager/src/pages/Login.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [formData, setFormData] = useState({ email: '', password: '' });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Login data:', formData);
+    // TODO: Call backend API to authenticate the user
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="email" name="email" placeholder="Email" onChange={handleChange} />
+        <input type="password" name="password" placeholder="Password" onChange={handleChange} />
+        <button type="submit">Log In</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
