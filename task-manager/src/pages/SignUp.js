9953a3ef54e8fa99f7863aee43677d0dfba48e7d
Risshub <rishavsubedi5@gmail.com>
2024-12-31 16:34:40 +0545

    Added Signup page with form and state management.

---
+++ task-manager/src/pages/SignUp.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const Signup = () => {
+  const [formData, setFormData] = useState({ name: '', email: '', password: '' });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Signup data:', formData);
+    // TODO: Call backend API to register the user
+  };
+
+  return (
+    <div>
+      <h1>Signup</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Name" onChange={handleChange} />
+        <input type="email" name="email" placeholder="Email" onChange={handleChange} />
+        <input type="password" name="password" placeholder="Password" onChange={handleChange} />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
